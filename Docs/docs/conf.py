ff7ddc4b7dc2661c3ea5548c6f9b549643249e92
hemanthpruthvi <hemanthpruthvi@github.com>
2020-10-17 19:02:02 +0530

    Modified documentation

--- Docs/docs/conf.py
+++ Docs/docs/conf.py
@@ -12,6 +12,8 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('../../PyAstroPol/'))
 import sphinx_rtd_theme
 
